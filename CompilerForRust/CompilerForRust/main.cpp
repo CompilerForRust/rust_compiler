2e2add5a0c9e9ba84f4d7dd42c5a3987e0fdfaee
1498886457 <51187382+1498886457@users.noreply.github.com>
2021-01-06 22:51:06 +0800

    最终完成

--- CompilerForRust/CompilerForRust/main.cpp
+++ CompilerForRust/CompilerForRust/main.cpp
@@ -1,9 +1,19 @@
 #include"Parser.h"
 #include <iostream>
 using namespace std;
+map<int, string>path;
+void InitMap() {
+	path[0] = "intTest.txt";
+	path[1] = "floatTest.txt";
+	path[2] = "forTest.txt";
+	path[3] = "functionCallTest.txt";
+	path[4] = "ifTest.txt";
+	path[5] = "whileTest.txt";
 
+}
 int main() {
-	string file = "floatTest.txt";
+	InitMap();
+	string file = path[5];
 	Parser parser(file);
 	parser.parse();
 
